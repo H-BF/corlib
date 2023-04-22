6337ca56264be177e7ce35dc582ee3b8f4ac9455
EvgenyGRI <eugene_gri@mail.ru>
2023-04-23 00:51:22 +0300

    fix panic in GRPS-HC service

--- server/api-server-hc.go
+++ server/api-server-hc.go
@@ -5,10 +5,11 @@ import (
 	"sync/atomic"
 
 	"github.com/H-BF/corlib/server/health_check"
+
 	"google.golang.org/grpc"
 )
 
-//HealthCheck optional to APIService interface
+// HealthCheck optional to APIService interface
 type HealthCheck interface {
 	HealthProbe(ctx context.Context) (*health_check.Response, error)
 }
@@ -19,23 +20,28 @@ type healthCheckService struct {
 	ready    atomic.Value
 }
 
+// Description -
 func (hc *healthCheckService) Description() grpc.ServiceDesc {
 	return health_check.ServiceDesc
 }
 
+// RegisterGRPC -
 func (hc *healthCheckService) RegisterGRPC(_ context.Context, srv *grpc.Server) error {
 	health_check.RegisterGRPC(srv, hc)
 	return nil
 }
 
+// OnStart -
 func (hc *healthCheckService) OnStart() {
 	hc.ready.Store(true)
 }
 
+// OnStop -
 func (hc *healthCheckService) OnStop() {
 	hc.ready.Store(false)
 }
 
+// HealthProbe -
 func (hc *healthCheckService) HealthProbe(_ context.Context) (*health_check.Response, error) {
 	resp := &health_check.Response{Status: health_check.StatusNotServing}
 	switch imReady := hc.ready.Load().(type) {
@@ -47,6 +53,7 @@ func (hc *healthCheckService) HealthProbe(_ context.Context) (*health_check.Resp
 	return resp, nil
 }
 
+// Check -
 func (hc *healthCheckService) Check(ctx context.Context, req *health_check.Request) (*health_check.Response, error) {
 	if ok, _ := hc.ready.Load().(bool); !ok {
 		return &health_check.Response{Status: health_check.StatusNotServing}, nil
@@ -56,7 +63,7 @@ func (hc *healthCheckService) Check(ctx context.Context, req *health_check.Reque
 	service := req.GetService()
 	if srv, ok := hc.services[service]; !ok {
 		resp = &health_check.Response{Status: health_check.StatusServiceUnknown}
-	} else if c, _ := srv.(HealthCheck); hc != nil {
+	} else if c, _ := srv.(HealthCheck); c != nil {
 		resp, err = c.HealthProbe(ctx)
 	} else {
 		resp, err = hc.HealthProbe(ctx)
