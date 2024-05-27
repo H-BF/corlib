91ccd7e6da5536888eecdf1b14cbfe3c260c24a7
EvgenyGRI <eugene_gri@mail.ru>
2024-05-27 19:00:54 +0300

    move some libs from sgroups repo

--- client/grpc/closable-conn.go
+++ client/grpc/closable-conn.go
@@ -4,18 +4,25 @@ import (
 	"io"
 
 	"github.com/H-BF/corlib/client/grpc/internal"
+
 	"google.golang.org/grpc"
 )
 
-//ErrConnClosed send when grpc is closed
+// ClientConn - grpc client connection
+type ClientConn interface {
+	grpc.ClientConnInterface
+	Close() error
+}
+
+// ErrConnClosed send when grpc is closed
 var ErrConnClosed = internal.ErrConnClosed
 
-//Closable is closable connect
+// Closable is closable connect
 type Closable interface {
 	CloseConn() error
 }
 
-//ClosableClientConnInterface grpc client conn interface with close ability
+// ClosableClientConnInterface grpc client conn interface with close ability
 type ClosableClientConnInterface interface {
 	grpc.ClientConnInterface
 	Closable
@@ -23,7 +30,7 @@ type ClosableClientConnInterface interface {
 
 var _ io.Closer = (*grpc.ClientConn)(nil) //assert
 
-//MakeCloseable ...
+// MakeCloseable ...
 func MakeCloseable(c grpc.ClientConnInterface) ClosableClientConnInterface {
 	if ret, ok := c.(ClosableClientConnInterface); ok {
 		return ret
